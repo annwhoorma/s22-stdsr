6b235a094bc8557dcf35b717b1d011deb34d5684
Anna Boronina <a.boronina@innopolis.university>
2022-02-12 18:01:52 +0300

    change full type to Fullness enum

--- assignment-1/new_algorithm.py
+++ assignment-1/new_algorithm.py
@@ -1,5 +1,6 @@
 from numpy import empty
-from mrl98 import Buffer, Element, Sequence, MRL98, plus_inf
+from mrl98 import Buffer, Element, Fullness, Sequence, MRL98, plus_inf, Fullness
+from mrl99 import MRL99
 
 '''
 This .py file implements the third (most efficient at 1998) version: New Algorithm:
@@ -14,10 +15,14 @@ Assign the output buffer, level e + 1"
 
 class NewAlgorithm:
     b = 3
-    def __init__(self, input_sequence: Sequence, b: int, be: int, phi: float):
-        self.mrl98 = MRL98(input_sequence, b, be)
+    def __init__(self, mrl_type: str, input_sequence: Sequence, b: int, be: int, phi: float):
+        assert '99' in mrl_type or '98' in mrl_type, 'mrl_type must contain 98 or 99'
+        self.mrl = MRL98(input_sequence, b, be) if '98' in mrl_type else MRL99(input_sequence, b, be)
+        # for MRL98 r is always 1 and not used
+        self.r = 2 if '99' in mrl_type else None
         self.input_sequence = input_sequence
         self.b = b
+        self.mrl_type = mrl_type
         self.be = be
         self.phi = phi
         self.buffers: list[Buffer] = []
@@ -31,34 +36,38 @@ class NewAlgorithm:
     def _count_empty_buffers(self) -> 'list[Buffer]':
         empty_buffers = []
         for buffer in self.buffers:
-            if not buffer.full:
+            if not buffer.full == Fullness.FULL:
                 empty_buffers.append(buffer)
         return empty_buffers, len(empty_buffers)
 
     def _update_l(self):
-        self.l = min(self.buffers, key=lambda buffer: buffer.level if buffer.full else plus_inf).level
+        self.l = min(self.buffers, key=lambda buffer: buffer.level if buffer.full == Fullness.FULL else plus_inf).level
 
     def _step(self):
         empty_buffers, empty_buffers_amount = self._count_empty_buffers()
         if empty_buffers_amount == 1:
             # invoke NEW on the empty buffer and assign it level `self.l`
             buffer = empty_buffers[0]
-            self.mrl98.new(buffer)
+            self.mrl.new(buffer) if '98' in self.mrl_type else self.mrl.new(buffer, self.r)
             buffer.update_level(self.l)
         elif empty_buffers_amount >= 2:
             # invoke NEW on each and assign level 0 to each one
             for buffer in empty_buffers:
-                self.mrl98.new(buffer)
-                buffer.update_level(0)
+                self.mrl.new(buffer) if '98' in self.mrl_type else self.mrl.new(buffer, self.r)
+                buffer.update_level(0 if '98' in self.mrl_type else 1)
         else:
             # invoke COLLAPSE on the set of buffers with level `self.l` and assign level `self.l`+1 to the output buffer
             min_buffers = list(filter(lambda buffer: buffer.level == self.l, self.buffers))
-            output_buffer = self.mrl98.collapse(min_buffers)
+            output_buffer = self.mrl.collapse(min_buffers)
             output_buffer.update_level(self.l + 1)
+            # tree height increases
+            if '99' in self.mrl_type:
+                self.r *= 2
         self._update_l()
 
     def run(self):
-        while len(self.input_sequence) != 0:
+        while len(self.input_sequence) > 0:
             self._step()
-        result = self.mrl98.output(self.phi, self.buffers)
+        
+        result = self.mrl.output(self.phi, self.buffers)
         return result
